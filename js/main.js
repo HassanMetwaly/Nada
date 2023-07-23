a2bc9e8ce8c28e952dadf3d853453cd0a056917b
HASSAN METWALY <131253773+HassanMetwaly@users.noreply.github.com>
2023-07-23 19:24:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let typed = new Typed('.text', {
+    strings: [
+        'Graphic designer',
+        'History teacher',
+        'Geography teacher'
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
